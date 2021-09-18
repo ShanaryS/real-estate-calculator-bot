d8f85ae61555319d6e5cef49909ea86a82b40611
ShanaryS <shanaryjs@gmail.com>
2021-09-17 21:49:48 -0400

    Added file to get current interest rate

--- get_current_interest_rate.py
+++ get_current_interest_rate.py
@@ -1,4 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
-import re
 
+url = "https://www.nerdwallet.com/mortgages/mortgage-rates"
+page = requests.get(url).text
+doc = BeautifulSoup(page, "html.parser")
+
+table = doc.find("tbody")
+interest_rate_30_years = float(str(table.td).split('>')[-2].split('%')[0]) / 100
+print(interest_rate_30_years)
