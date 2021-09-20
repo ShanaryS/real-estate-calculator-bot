b25afd975b7887536a2e0af9c908d1ba007315f8
ShanaryS <shanaryjs@gmail.com>
2021-09-19 23:13:14 -0400

    Completed all logic for adding urls

--- add_urls.py
+++ add_urls.py
@@ -6,18 +6,24 @@ from colors_for_print import PrintColors
 
 # Stores the urls from inputs. Gets written to file after add_link() is completed. If was cancelled, it gets cleared.
 urls = set()
-overwrite = search = delete = False
+
+# Variables that dictate how each option that the user chooses is handled
+to_overwrite = is_search = to_delete = False
+s_p, search, property_, a_o_d = 's_p', 's', 'p', 'a_o_d'
+append, overwrite, delete, cancel, exe = 'a', 'o', 'd', 'c', 'e'
+
+# Used for delaying terminating program so user can read final text
 sleep_timer = 1
 
 
 def quit_program() -> None:
     """Quits programing without saving an data"""
 
-    global overwrite, search, delete
-    overwrite = search = delete = False
+    global to_overwrite, is_search, to_delete
+    to_overwrite = is_search = to_delete = False
     urls.clear()
 
-    print_captions(c=True)
+    print_captions(mode=cancel)
     time.sleep(sleep_timer)
 
 
@@ -30,11 +36,11 @@ def url_is_valid(url) -> bool:
 
     # If a property URL, return depending on mode user picked.
     if url[:27] == 'https://www.zillow.com/home' and len(url) >= 35:
-        if search:
+        if is_search:
             return False
         return True
     else:
-        if search:
+        if is_search:
             return True
         return False
 
@@ -43,275 +49,163 @@ def commit_updates_to_file() -> None:
     """Commits changes to file"""
 
     if urls:
-        save_urls(urls, overwrite=overwrite, search=search, delete=delete)
+        save_urls(urls, overwrite=to_overwrite, search=is_search, delete=to_delete)
 
     print_captions(execute=True)
     time.sleep(sleep_timer)
 
 
-def print_captions(s_p=False, a_o_d=False, a=False, o=False, d=False, e=False,
-                   c=False, valid=True, received=False, execute=False) -> None:
+def print_captions(mode=None, e=False, valid=True, received=False, execute=False) -> None:
     """Prints text that tells the user what the programing is doing"""
 
     BAD, OK, GOOD, GREAT = PrintColors.FAIL, PrintColors.WARNING, PrintColors.OKCYAN, PrintColors.OKGREEN
     END = PrintColors.ENDC
 
-    if s_p:
+    if mode == 's_p':
         print(f"{GOOD}Do you want to update search URLs 's' or property URLs 'p'? ('c' to cancel):{END}", end=" ")
-    elif a_o_d:
+    elif mode == 'a_o_d':
         print(f"{GOOD}Do you want to append 'a', overwrite 'o', or delete 'd'? ('c' to cancel):{END}", end=" ")
-    elif a:
+    elif mode == 'a':
         print(f"\n{OK}--- APPEND MODE... URLs in this session will be appended to file! ---{END}")
         if e:
             print(f"{GOOD}Enter another URL to append ('e' to execute changes, 'c' to cancel):{END}", end=" ")
         else:
             print(f"{GOOD}- Enter URL to append ('c' to cancel):{END}", end=" ")
-    elif o:
+    elif mode == 'o':
         print(f"\n{OK}--- OVERWRITE MODE... URLs before this session will be lost! ---{END}")
         if e:
             print(f"{GOOD}Enter another URL to write ('e' to execute changes, 'c' to cancel):{END}", end=" ")
         else:
             print(f"{GOOD}Enter URL to write ('c' to cancel):{END}", end=" ")
-    elif d:
+    elif mode == 'd':
         print(f"\n{OK}--- DELETE MODE... URLs in session will be lost! ---{END}")
         if e:
             print(f"{GOOD}Enter another URL to delete ('e' to execute changes, 'c' to cancel):{END}", end=" ")
         else:
             print(f"{GOOD}Enter URL to delete ('c' to cancel):{END}", end=" ")
-    elif c:
-        print(f"{BAD}!!! Ending program... No changes were made! !!!{END}")
+    elif mode == 'c':
+        print(f"\n{BAD}!!! No changes were made! Ending program... !!!{END}")
 
     elif not valid:
-        print(f"{BAD}!!! Invalid URL... Try again! (Correct URL for Search/Property?) !!!{END}")
+        print(f"\n{BAD}!!! Invalid URL... Try again! (Correct URL for Search/Property?) !!!{END}")
     elif received:
-        print(f"{GREAT}!!! URL received! !!!{END}")
+        print(f"\n{GREAT}!!! URL received! !!!{END}")
     elif execute:
-        print(f"{GREAT}!!! Committed changes to file! !!!{END}")
+        print(f"\n{GREAT}!!! Committed changes to file! Ending program... !!!{END}")
 
 
-def add_link() -> None:
+def add_link(mode) -> None:
     """Logic for adding URLs"""
 
-    global overwrite, search, delete
-    print_captions(s_p=True)
+    global is_search
+
+    print_captions(mode=mode)
     search_or_property = input()
-    while search_or_property != 's' and search_or_property != 'p' and search_or_property != 'c':
-        print_captions(s_p=True)
+    while search_or_property != search and search_or_property != property_ and search_or_property != cancel:
+        print_captions(mode=mode)
         search_or_property = input()
 
-    if search_or_property != 'c':
-
-        if search_or_property == 's':
-            search = True
-
-            print_captions(a_o_d=True)
-            append_overwrite_delete = input()
-            while append_overwrite_delete != 'a' and append_overwrite_delete != 'o'\
-                    and append_overwrite_delete != 'd' and append_overwrite_delete != 'c':
-                print_captions(a_o_d=True)
-                append_overwrite_delete = input()
-
-            if append_overwrite_delete != 'c':
-
-                if append_overwrite_delete == 'a':
-                    overwrite = False
-
-                    print_captions(a=True)
-                    new_url = get_url_from_input()
-
-                    if new_url != 'c':
-                        valid = url_is_valid(new_url)
-                        while not valid:
-                            print_captions(valid=False)
-
-                            print_captions(a=True)
-                            new_url = get_url_from_input()
-
-                            if new_url == 'c':
-                                quit_program()
-                                return
-
-                            valid = url_is_valid(new_url)
-
-                        urls.add(new_url)
-                        print_captions(received=True)
-
-                        print_captions(a=True, e=True)
-                        new_url = get_url_from_input()
-                        while new_url != 'c':
-
-                            if new_url == 'e':
-                                commit_updates_to_file()
-                                return
-
-                            else:
-                                valid = url_is_valid(new_url)
-                                while not valid:
-                                    print_captions(valid=False)
-
-                                    print_captions(a=True, e=True)
-                                    new_url = get_url_from_input()
-
-                                    if new_url == 'e':
-                                        commit_updates_to_file()
-                                        return
-
-                                    if new_url == 'c':
-                                        quit_program()
-                                        return
-
-                                    valid = url_is_valid(new_url)
-
-                                urls.add(new_url)
-                                print_captions(received=True)
-
-                            print_captions(a=True, e=True)
-                            new_url = get_url_from_input()
-
-                        if new_url == 'c':
-                            quit_program()
-                            return
-
-                    elif new_url == 'c':
-                        quit_program()
-                        return
-
-                elif append_overwrite_delete == 'o':
-                    overwrite = True
-
-                    print_captions(o=True)
-                    new_url = get_url_from_input()
-
-                    if new_url != 'c':
-                        valid = url_is_valid(new_url)
-                        while not valid:
-                            print_captions(valid=False)
-
-                            print_captions(o=True)
-                            new_url = get_url_from_input()
-
-                            if new_url == 'c':
-                                quit_program()
-                                return
-
-                            valid = url_is_valid(new_url)
-
-                        urls.add(new_url)
-                        print_captions(received=True)
-
-                        print_captions(o=True, e=True)
-                        new_url = get_url_from_input()
-                        while new_url != 'c':
-
-                            if new_url == 'e':
-                                commit_updates_to_file()
-                                return
-
-                            else:
-                                valid = url_is_valid(new_url)
-                                while not valid:
-                                    print_captions(valid=False)
-
-                                    print_captions(o=True, e=True)
-                                    new_url = get_url_from_input()
-
-                                    if new_url == 'e':
-                                        commit_updates_to_file()
-                                        return
-
-                                    if new_url == 'c':
-                                        quit_program()
-                                        return
-
-                                    valid = url_is_valid(new_url)
-
-                                urls.add(new_url)
-                                print_captions(received=True)
-
-                            print_captions(o=True, e=True)
-                            new_url = get_url_from_input()
-
-                        if new_url == 'c':
-                            quit_program()
-                            return
-
-                    elif new_url == 'c':
-                        quit_program()
-                        return
-
-                elif append_overwrite_delete == 'd':
-                    delete = True
-
-                    print_captions(d=True)
-                    new_url = get_url_from_input()
-
-                    if new_url != 'c':
-                        valid = url_is_valid(new_url)
-                        while not valid:
-                            print_captions(valid=False)
-
-                            print_captions(d=True)
-                            new_url = get_url_from_input()
-
-                            if new_url == 'c':
-                                quit_program()
-                                return
-
-                            valid = url_is_valid(new_url)
-
-                        urls.add(new_url)
-                        print_captions(received=True)
-
-                        print_captions(d=True, e=True)
-                        new_url = get_url_from_input()
-                        while new_url != 'c':
-
-                            if new_url == 'e':
-                                commit_updates_to_file()
-                                return
-
-                            else:
-                                valid = url_is_valid(new_url)
-                                while not valid:
-                                    print_captions(valid=False)
-
-                                    print_captions(d=True, e=True)
-                                    new_url = get_url_from_input()
-
-                                    if new_url == 'e':
-                                        commit_updates_to_file()
-                                        return
-
-                                    if new_url == 'c':
-                                        quit_program()
-                                        return
-
-                                    valid = url_is_valid(new_url)
-
-                                urls.add(new_url)
-                                print_captions(received=True)
-
-                            print_captions(d=True, e=True)
-                            new_url = get_url_from_input()
-
-                        if new_url == 'c':
-                            quit_program()
-                            return
-
-                    elif new_url == 'c':
-                        quit_program()
-                        return
-
-            elif append_overwrite_delete == 'c':
-                quit_program()
-                return
-
-        elif search_or_property == 'p':
-            print('test')
-
-    elif search_or_property == 'c':
+    if search_or_property != cancel:
+        if search_or_property == search:
+            is_search = True
+            _choose_options(a_o_d)
+        elif search_or_property == property_:
+            is_search = False
+            _choose_options(a_o_d)
+    elif search_or_property == cancel:
         quit_program()
         return
 
 
-add_link()
+def _choose_options(mode) -> None:
+    """Handles the user choosing different options for URLs"""
+
+    global to_overwrite, to_delete
+
+    print_captions(mode=mode)
+    append_overwrite_delete = input()
+    while append_overwrite_delete != append and append_overwrite_delete != overwrite \
+            and append_overwrite_delete != delete and append_overwrite_delete != cancel:
+        print_captions(mode=mode)
+        append_overwrite_delete = input()
+
+    if append_overwrite_delete != cancel:
+        if append_overwrite_delete == append:
+            to_overwrite = False
+            _get_urls_from_input(append)
+        elif append_overwrite_delete == overwrite:
+            to_overwrite = True
+            _get_urls_from_input(overwrite)
+        elif append_overwrite_delete == delete:
+            to_delete = True
+            _get_urls_from_input(delete)
+    elif append_overwrite_delete == cancel:
+        quit_program()
+        return
+
+
+def _get_urls_from_input(mode) -> None:
+    """Gets URLs from user. Calls functions that adds them to file"""
+
+    print_captions(mode=mode)
+    new_url = get_url_from_input()
+
+    if new_url != cancel:
+        valid = url_is_valid(new_url)
+        while not valid:
+            print_captions(valid=False)
+
+            print_captions(mode=mode)
+            new_url = get_url_from_input()
+
+            if new_url == cancel:
+                quit_program()
+                return
+
+            valid = url_is_valid(new_url)
+
+        urls.add(new_url)
+        print_captions(received=True)
+
+        print_captions(mode=mode, e=True)
+        new_url = get_url_from_input()
+        while new_url != cancel:
+
+            if new_url == exe:
+                commit_updates_to_file()
+                return
+
+            else:
+                valid = url_is_valid(new_url)
+                while not valid:
+                    print_captions(valid=False)
+
+                    print_captions(mode=mode, e=True)
+                    new_url = get_url_from_input()
+
+                    if new_url == exe:
+                        commit_updates_to_file()
+                        return
+
+                    if new_url == cancel:
+                        quit_program()
+                        return
+
+                    valid = url_is_valid(new_url)
+
+                urls.add(new_url)
+                print_captions(received=True)
+
+            print_captions(mode=mode, e=True)
+            new_url = get_url_from_input()
+
+        if new_url == cancel:
+            quit_program()
+            return
+
+    elif new_url == cancel:
+        quit_program()
+        return
+
+
+add_link(s_p)
