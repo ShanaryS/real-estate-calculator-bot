0b7e249fd7520b16bb5943429cb65fa047681969
ShanaryS <shanaryjs@gmail.com>
2021-09-21 16:40:00 -0400

    Cleaned up code

--- webscrapers/get_property_urls_from_search.py
+++ webscrapers/get_property_urls_from_search.py
@@ -1,4 +1,5 @@
 """Web scrapes properties from zillow search URL."""
+import bs4.element
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -6,15 +7,22 @@ import requests
 import time
 
 
+# Delay between actions for selenium driver
+SCROLL_DELAY = 0.05
+PAGE_LOAD_WAIT = 1
+HOLD_LENGTH = 5
+REDIRECT_WAIT = 10
+
 PROPERTIES_PER_PAGE = 40  # Number of properties zillow displays per search page
-DRIVER_DELAY = 0.05  # Delay between actions for selenium driver
+
+# Global variables all functions use
 url_search: str
 chrome: webdriver.Chrome
 zillow: BeautifulSoup
 
 
 def is_url_valid(url) -> bool:
-    """Checks if URL was incorrectly inputted by looking for an error page"""
+    """Checks if URL was incorrectly inputted by looking for an error page. For both individual properties and search"""
 
     # Zillow has bot detection. This handles it.
     req_headers = {
@@ -36,12 +44,17 @@ def is_url_valid(url) -> bool:
     return valid
 
 
+def get_url() -> str:
+    """Returns URL for property"""
+    return url_search
+
+
 def set_page_search(url) -> None:
     """Opens chromedriver using selenium"""
 
     global url_search, chrome, zillow
 
-    url_search = _set_url_search(url)
+    url_search = url
 
     chrome = webdriver.Chrome()
     chrome.get(url_search)
@@ -52,49 +65,52 @@ def set_page_search(url) -> None:
     zillow = BeautifulSoup(zillow_page, 'html.parser')
 
 
-def _set_url_search(url=None) -> str:
-    """Gets zillow search URL from user or file when running analysis"""
-
-    if url:
-        _url = url
-    else:
-        _url = str(input("Enter full URL from zillow search: "))
-
-        while _url[:23] != 'https://www.zillow.com/' or len(_url) < 29:
-            _url = str(input("Enter full URL from zillow search: "))
-
-    return _url
-
-
 def _scroll_to_page_bottom() -> None:
     """Runs chrome browser with selenium and scrolls to bottom of page to load all JS elements"""
 
-    time.sleep(1)  # Give time for the page to fully load. Though it should without, being cautious.
+    time.sleep(PAGE_LOAD_WAIT)  # Give time for the page to fully load. Though it should without, being cautious.
 
     # Handles zillow captcha that sometimes appear
     # Use this url to test captcha
     # chrome.get('https://www.zillow.com/captchaPerimeterX/?url=%2fhomes%2fCT_rb%2f&uuid=dd265dba-1ac2-11ec-a883-615050666d69&vid=')
     if 'captcha' in chrome.current_url.lower():
+        _solve_captcha()
 
-        target = chrome.find_element_by_id('px-captcha')
-        action = webdriver.ActionChains(chrome)
-        action.click_and_hold(on_element=target)
-        action.perform()
-        time.sleep(5)  # Holds button for 5 seconds. Should cover all the variable lengths
-
-        action.release(on_element=target)
-        action.perform()
-        time.sleep(10)  # Wait for redirect from captcha. Can some times be long.
-
+    # Clicks presses page down multiple times to scroll to bottom of page.
     target = chrome.find_element_by_tag_name("body")
     for i in range(10):
         target.send_keys(Keys.PAGE_DOWN)
-        time.sleep(DRIVER_DELAY)
+        time.sleep(SCROLL_DELAY)
 
 
-def get_url() -> str:
-    """Returns URL for property"""
-    return url_search
+def _solve_captcha() -> None:
+    """Solves the captcha"""
+
+    target = chrome.find_element_by_id('px-captcha')
+    action = webdriver.ActionChains(chrome)
+    action.click_and_hold(on_element=target)
+    action.perform()
+    time.sleep(HOLD_LENGTH)  # Holds button for 5 seconds. Should cover all the variable lengths
+
+    action.release(on_element=target)
+    action.perform()
+    time.sleep(REDIRECT_WAIT)  # Wait for redirect from captcha. Can some times be long.
+
+
+def _set_url_to_first_page() -> str:
+    """Makes sure that the url given starts on first page"""
+
+    current_page_num = _get_current_page(url_search)
+
+    if current_page_num == 1:
+        first_page_url = url_search
+    else:
+        temp = url_search.split('/')
+        temp.pop(4)
+        temp[4] = temp[4].replace(f'%22pagination%22%3A%7B%22currentPage%22%3A{current_page_num}%7D%2C', '')
+        first_page_url = '/'.join(temp)
+
+    return first_page_url
 
 
 def _get_current_page(url) -> int:
@@ -108,26 +124,10 @@ def _get_current_page(url) -> int:
     return current_page_num
 
 
-def _set_url_to_first_page(url) -> str:
-    """Makes sure that the url given starts on first page"""
-
-    current_page_num = _get_current_page(url)
-
-    if current_page_num == 1:
-        first_page_url = url
-    else:
-        temp = url.split('/')
-        temp.pop(4)
-        temp[4] = temp[4].replace(f'%22pagination%22%3A%7B%22currentPage%22%3A{current_page_num}%7D%2C', '')
-        first_page_url = '/'.join(temp)
-
-    return first_page_url
-
-
 def _get_num_pages_and_listings() -> tuple:
     """Returns the number of pages in the search"""
 
-    # Checks if looking at agent listings or other listings. Other listings will always have 'cat2' in url.
+    # Checks if looking at Agent listings or Other listings. Other listings will always have 'cat2' in url.
     if 'cat2' not in url_search:
         listings = int(zillow.find_all(class_="total-text")[0].string.replace(',', ''))
         num_pages = -(-listings // PROPERTIES_PER_PAGE)  # Ceiling division
@@ -158,7 +158,7 @@ def _get_url_for_next_page(url) -> str:
     return next_page_url
 
 
-def _get_property_url_from_search(li) -> str:
+def _get_property_url_from_search(li: bs4.element.Tag) -> str:
     """Gets url for property from search url"""
 
     property_url = li.find('a', href=True)['href']
@@ -166,7 +166,7 @@ def _get_property_url_from_search(li) -> str:
     return property_url
 
 
-def _get_price_from_search(li) -> int:
+def _get_price_from_search(li: bs4.element.Tag) -> int:
     """Gets price for property from search url"""
 
     price = int(li.find('div', class_="list-card-price").string.lstrip('$').replace(',', ''))
