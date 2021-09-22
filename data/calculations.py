ced60c862008c948f2499432e694623021a3b9f0
ShanaryS <shanaryjs@gmail.com>
2021-09-22 15:53:50 -0400

    Added logging errors to errors.log

--- data/calculations.py
+++ data/calculations.py
@@ -50,7 +50,7 @@ def update_values(url=None, save_to_file=True, update_interest_rate=True) -> boo
 
         # "###" can be used to navigate between errors in log file.
         error = f"### {url}: [\n" \
-                f"{e}" \
+                f"{e}\n" \
                 "]\n\n"
 
         with open(os.path.join('output', 'errors.log'), 'a') as file:
