7a414de0211fcdac79b149cf39226643f6aae7dc
ShanaryS <shanaryjs@gmail.com>
2021-09-23 00:17:02 -0400

    Cleaned up code

--- data/user.py
+++ data/user.py
@@ -1,26 +1,11 @@
 """Handles updating necessary variables per user input"""
 
 
+from values import *
 from web.get_current_interest_rates import set_page_interest_rates, interest_rates
 from web.get_property_info import get_address, get_price, get_year, get_description, get_property_taxes,\
     get_num_units, get_rent_per_unit, get_sqft, get_price_per_sqft, get_lot_size, get_parking
 
-
-'''DEFAULT VALUES USED FOR ANALYSIS. CHANGE THESE VALUES ACCORDING TO YOUR HEURISTICS. TOUCH NOTHING ELSE.'''
-DOWN_PAYMENT_PERCENT = 0.20
-YEARS = 30  # 'FHA' and 'VA' requires YEARS = 30. 'Conventional' has options for YEARS = 30, 20, 15, 10.
-LOAN_TYPE = 'Conventional'  # Options are 'Conventional', 'FHA', 'VA'. !!! NOTE: 'FHA' and 'VA' requires YEARS = 30.
-PROPERTY_TAXES = 4000
-FIX_UP_COST = 10000
-NUM_UNITS = 3
-RENT_PER_UNIT = 700  # Never used. use_default_rent_per_unit() uses a bit more sophistication in estimation
-VACANCY_PERCENT = 0.08
-MAINTENANCE_PERCENT = 0.15
-MANAGEMENT_PERCENT = 0.10
-DEPRECIATION_SHORT_PERCENT = 0.08
-DEPRECIATION_LONG_PERCENT = 0.75
-TAX_BRACKET = 0.24
-IS_FIRST_RENTAL = True
 
 # Variables to keep track if values could not be found and thus used a default value
 found_property_taxes = True
