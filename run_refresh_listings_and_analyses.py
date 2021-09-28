5c9360f858b44a9e5d429b8ffbade17b736ea333
ShanaryS <shanaryjs@gmail.com>
2021-09-28 02:24:11 -0400

    Cleaned up code

--- run_refresh_listings_and_analyses.py
+++ run_refresh_listings_and_analyses.py
@@ -1,15 +1,15 @@
 """This allows refreshing the listings from all the search URLs without requiring user input. Useful for automation."""
 
 from run_property_tracker import add_link
-from run_analyses import main
+from run_analyses import main as run
 
 
-def run() -> None:
+def main() -> None:
     """Main function"""
 
     add_link(refresh_no_input=True)
-    main()
+    run()
 
 
 if __name__ == '__main__':
-    run()
+    main()
