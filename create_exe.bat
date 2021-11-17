2e7c7d30f8fca0082d23b24d20d0fe711b6f3fa8
Shanary <86130442+ShanaryS@users.noreply.github.com>
2021-11-17 12:36:37 -0500

    Update create_exe.bat

--- create_exe.bat
+++ create_exe.bat
@@ -1,27 +1,36 @@
-set name="Property Tracker"
+@echo off
 
-venv\Scripts\pyinstaller.exe -F -n %name% --distpath ./bin -p "venv\Lib\site-packages" --noconsole run_property_tracker.py
+set name="Property Tracker"
+set src="run_property_tracker.py"
+
+venv\Scripts\pyinstaller.exe -F -n %name% -p "venv\Lib\site-packages" --distpath ./bin %src%
 
 del %name%".spec"
 rmdir /s /q "__pycache__" "build"
+
 
 set name="Analyze Properties"
+set src="run_analyses.py"
 
-venv\Scripts\pyinstaller.exe -F -n %name% --distpath ./bin -p "venv\Lib\site-packages" --noconsole --noconsole run_analyses.py
+venv\Scripts\pyinstaller.exe -F -n %name% -p "venv\Lib\site-packages" --distpath ./bin %src%
 
 del %name%".spec"
 rmdir /s /q "__pycache__" "build"
+
 
 set name="Refresh Listings & Analyses"
+set src="run_refresh_listings_and_analyses.py"
 
-venv\Scripts\pyinstaller.exe -F -n %name% --distpath ./bin -p "venv\Lib\site-packages" --noconsole --noconsole run_refresh_listings_and_analyses.py
+venv\Scripts\pyinstaller.exe -F -n %name% -p "venv\Lib\site-packages" --distpath ./bin %src%
 
 del %name%".spec"
 rmdir /s /q "__pycache__" "build"
 
-set name="Print Single Property Analysis"
 
-venv\Scripts\pyinstaller.exe -F -n %name% --distpath ./bin -p "venv\Lib\site-packages" --noconsole --noconsole run_single_property_analysis_print_only.py
+set name="Print Single Property Analysis"
+set src="run_single_property_analysis_print_only.py"
+
+venv\Scripts\pyinstaller.exe -F -n %name% -p "venv\Lib\site-packages" --distpath ./bin %src%
 
 del %name%".spec"
 rmdir /s /q "__pycache__" "build"
