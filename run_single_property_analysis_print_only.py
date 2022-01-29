6bdf00acf790bf0cd77ac81a70224ebf0a85bffa
ShanaryS <shanaryjs@gmail.com>
2022-01-28 20:02:38 -0500

    handle erorr gracefully

--- run_single_property_analysis_print_only.py
+++ run_single_property_analysis_print_only.py
@@ -4,12 +4,18 @@ Useful if only need to check a single property.
 
 from src.data.calculations import update_values, print_property_info, \
     print_analysis, print_sql_amortization_table
+from src.data.colors_for_print import BAD, OK, GOOD, GREAT, END
 
 
 def main() -> None:
     """Main function"""
 
-    update_values(save_to_file=False)
+    if not update_values(save_to_file=False):
+        print(f" {BAD}!!! ERROR ANALYZING THIS PROPERTY. "
+              f"CHECK \\output\\errors.log FOR DETAILS. !!!{END}"
+              )
+        input('Press Enter to close program...')
+        return
     print_sql_amortization_table()
     print_property_info()
     print_analysis()
