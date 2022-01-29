9ea7389a22ef224db2ccaa9f98e3c0e4e0e0b6b6
ShanaryS <shanaryjs@gmail.com>
2022-01-28 19:27:52 -0500

    sign .exe

--- create_exe.bat
+++ create_exe.bat
@@ -1,6 +1,8 @@
 set name="Property Tracker"
 
 venv\Scripts\pyinstaller.exe -F -n %name% --distpath ./bin -p "venv\Lib\site-packages" --noconsole run_property_tracker.py
+signtool sign /a bin\\%name%.exe
+signtool timestamp /t http://timestamp.digicert.com bin\\%name%.exe
 
 del %name%".spec"
 rmdir /s /q "__pycache__" "build"
@@ -8,6 +10,8 @@ rmdir /s /q "__pycache__" "build"
 set name="Analyze Properties"
 
 venv\Scripts\pyinstaller.exe -F -n %name% --distpath ./bin -p "venv\Lib\site-packages" --noconsole --noconsole run_analyses.py
+signtool sign /a bin\\%name%.exe
+signtool timestamp /t http://timestamp.digicert.com bin\\%name%.exe
 
 del %name%".spec"
 rmdir /s /q "__pycache__" "build"
@@ -15,6 +19,8 @@ rmdir /s /q "__pycache__" "build"
 set name="Refresh Listings & Analyses"
 
 venv\Scripts\pyinstaller.exe -F -n %name% --distpath ./bin -p "venv\Lib\site-packages" --noconsole --noconsole run_refresh_listings_and_analyses.py
+signtool sign /a bin\\%name%.exe
+signtool timestamp /t http://timestamp.digicert.com bin\\%name%.exe
 
 del %name%".spec"
 rmdir /s /q "__pycache__" "build"
@@ -22,6 +28,8 @@ rmdir /s /q "__pycache__" "build"
 set name="Print Single Property Analysis"
 
 venv\Scripts\pyinstaller.exe -F -n %name% --distpath ./bin -p "venv\Lib\site-packages" --noconsole --noconsole run_single_property_analysis_print_only.py
+signtool sign /a bin\\%name%.exe
+signtool timestamp /t http://timestamp.digicert.com bin\\%name%.exe
 
 del %name%".spec"
 rmdir /s /q "__pycache__" "build"
